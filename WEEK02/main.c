572d0e5c72b0876cde1ebf8c1ef9aae5caeb2930
seesses <woosa0423@gmail.com>
2024-09-11 14:24:38 +0900

    summation

--- WEEK02/main.c
+++ WEEK02/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
 	
+	int a, b;	//정수 변수 선언 
+	
 	printf("Hello, Sookmyung Electronics!\n");
+	
+	a=10;
+	b=20;
+	printf("%d + %d = %d\n", a, b, a+b);
+	 
 	return 0;
 }
